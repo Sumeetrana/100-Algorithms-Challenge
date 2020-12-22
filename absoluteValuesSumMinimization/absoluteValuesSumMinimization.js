118669e81b290290754f347ff66a54c20c20024c
Sumeetrana <sumeetsinh28@gmail.com>
2020-12-22 18:07:37 +0530

    absoluteValuesSumMinimization

---
+++ absoluteValuesSumMinimization/absoluteValuesSumMinimization.js
@@ -0,0 +1,5 @@
+function absoluteValueSumMinimization(a) {
+    var isEven = a.length % 2 === 0;
+    return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
+}
+console.log(absoluteValueSumMinimization([2, 4, 7]));
